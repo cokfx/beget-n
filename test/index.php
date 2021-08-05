cd10b815116fa6b09545231e78c9063c8c92c5ac
cokfx <av230267@ya.ru>
2021-08-05 23:43:58 +0300

    5555555555555

--- test/index.php
+++ test/index.php
@@ -3,6 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Новый раздел");
 ?>
 
-    Text here....
+   55555555555555555555
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
