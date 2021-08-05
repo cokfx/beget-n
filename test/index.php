6d7f8d8181a914fcaafe32f99a40ca287aac4fb5
cokfx <av230267@ya.ru>
2021-08-05 23:46:49 +0300

    222222222222222

--- test/index.php
+++ test/index.php
@@ -3,6 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Новый раздел");
 ?>
 
-    Text here....
+    2222222222
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
