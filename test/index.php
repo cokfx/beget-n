c68bf15aa239c7187a2883b61dddf66c1fb3d159
cokfx <230267@bk.ru>
2021-08-06 07:43:52 +0300

    Update index.php

--- test/index.php
+++ test/index.php
@@ -3,7 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Новый раздел");
 ?>
 
-    000000000000000000
    55555555555555555555
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
