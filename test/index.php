b8696cda1871a94fdca26353b726eb07f02447dc
cokfx <av230267@ya.ru>
2021-08-06 19:09:21 +0300

    todo

--- test/index.php
+++ test/index.php
@@ -1,8 +1,12 @@
-<?
+<?php
+/**
+ * @params
+ */
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+global $APPLICATION;
 $APPLICATION->SetTitle("Новый раздел");
 ?>
 
    555555555555555555556666666666666666666
-
-<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+<?php // TODO sdfdfsfdsf ?>
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
